1defb0aa942e645176aa1f3297cb73ce43f8ff23
谭崇斌 <1736000171@qq.com>
2024-03-19 10:33:09 +0800

    Remove sensitive information

--- src/config/multer.config.ts
+++ src/config/multer.config.ts
@@ -1,8 +1,3 @@
 export default {
-  accessKeyId: "",
-  accessKeySecret: "",
-  bucket: "xcyl-miniprogram",
-  region: "oss-cn-hangzhou",
-  jwtKey:'asljdjflskjkalsjds', // token密钥
-  expiresIn:'1d' // token过期时间
+
 };
