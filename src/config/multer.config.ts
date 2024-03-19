93e825df05d22f8642647ec2112bb9d515823e38
Melody <1736000171@qq.com>
2024-03-19 02:34:29 +0000

    update src/config/multer.config.ts.
    Signed-off-by: Melody <1736000171@qq.com>

--- src/config/multer.config.ts
+++ src/config/multer.config.ts
@@ -1,6 +1,6 @@
 export default {
-  accessKeyId: "LTAI5tHCe1qXxWuVrcXie2vA",
-  accessKeySecret: "i0ePMyQsibdV4oya3Brgi7XRYqLCch",
+  accessKeyId: "",
+  accessKeySecret: "",
   bucket: "xcyl-miniprogram",
   region: "oss-cn-hangzhou",
   jwtKey:'asljdjflskjkalsjds', // token密钥
